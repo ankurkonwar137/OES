1b1f6ddaf02c1ff859c59209ebe47c7d88621532
Ankur Jyoti Konwar <ankur137@iitg.ac.in>
2025-03-10 17:32:02 +0530

    Changes done in various file2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,4 @@
 
-// $(document).ready(function () {
-//     let slideIndex = 0;
-//     const $slides = $(".slide");
-//     const slideCount = $slides.length;
-//     let slideInterval;
-
-//     // Dynamically create dots based on the number of slides
-//     const $dotsContainer = $(".dots-container");
-//     $dotsContainer.empty(); // Clear existing dots
-
-//     for (let i = 0; i < slideCount; i++) {
-//         $dotsContainer.append(`<span class="dot" data-index="${i}"></span>`);
-//     }
-
-//     const $dots = $(".dot");
-
-//     // Function to show slides
-//     function showSlide(index) {
-//         if (index >= slideCount) slideIndex = 0;
-//         if (index < 0) slideIndex = slideCount - 1;
-
-//         $(".carousel-container").css("transform", `translateX(${-slideIndex * 100}%)`);
-
-//         $dots.removeClass("active");
-//         $dots.eq(slideIndex).addClass("active");
-//     }
-
-//     // Next slide function
-//     function nextSlide() {
-//         slideIndex++;
-//         showSlide(slideIndex);
-//     }
-
-//     // Previous slide function
-//     function prevSlide() {
-//         slideIndex--;
-//         showSlide(slideIndex);
-//     }
-
-//     // Function to go to a specific slide
-//     function currentSlide(index) {
-//         slideIndex = index;
-//         showSlide(slideIndex);
-//     }
-
-//     // Start auto-slide
-//     function startSlideShow() {
-//         slideInterval = setInterval(nextSlide, 3000);
-//     }
-
-//     // Stop auto-slide on hover
-//     function stopSlideShow() {
-//         clearInterval(slideInterval);
-//     }
-
-//     // Event listeners
-//     $(".prev").click(prevSlide);
-//     $(".next").click(nextSlide);
-//     $dotsContainer.on("click", ".dot", function () {
-//         currentSlide($(this).data("index"));
-//     });
-
-//     $(".carousel-container").hover(stopSlideShow, startSlideShow);
-
-//     // Initialize slider
-//     startSlideShow();
-//     showSlide(slideIndex);
-// });
-
 $(document).ready(function () {
     let slideIndex = 0;
     const $slides = $(".slide img");
@@ -104,12 +35,26 @@ $(document).ready(function () {
         showSlide(slideIndex);
     }
 
-    // Initialize slider
-    updateArrowImages(); // Set initial images
-    showSlide(slideIndex);
+    // Function to start automatic sliding
+    function startSlideShow() {
+        slideInterval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
+    }
 
+    // Function to stop auto sliding (on hover)
+    function stopSlideShow() {
+        clearInterval(slideInterval);
+    }
+
+    // Initialize slider
+    updateArrowImages();
+    showSlide(slideIndex);
+    startSlideShow(); // Start automatic sliding
+
+    // Event listeners
     $(".prev").click(prevSlide);
     $(".next").click(nextSlide);
-});
 
+    // Stop autoplay when hovering over the carousel
+    $(".carousel-container").hover(stopSlideShow, startSlideShow);
+});
 
